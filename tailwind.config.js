ff9313cc53ab2492a37326a3948479a22c0590cd
webtensei <webtensei@gmail.com>
2023-08-20 15:00:15 +0700

    Dark mode + extend custom FF GTR + posts image update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html'],
+   darkMode:'class',
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows:{
+        'layout':'auto 1fr auto',
+      },
+      fontFamily:{
+        main:['Karla']
+      }
+    },
   },
   plugins: [],
 }
